115bdae75e22b7848d96c8dabcd1488958474b46
amhclark <amhclark@mun.ca>
2024-02-07 19:33:50 -0330

    Functional CLT code

--- clt_abd.m
+++ clt_abd.m
@@ -6,38 +6,53 @@ clear
 close all
 clc
 
-N = 10;                 % Number of Layers
-E1 = 100;               % Modulus of Elasticity
-E2 = 100;
-nu_12 = 0.25;           % Poisson's Ratio
+% N = 10;               % Number of Layers
+E1 = 133.44;            % Modulus of Elasticity
+E2 = 8.78;
+nu_12 = 0.26;           % Poisson's Ratio
 nu21 = (E2*nu_12)/E1;
-G12 = 100;              % Bulk Modulus
+G12 = 3.254;            % Bulk Modulus
 
-theta = [theta1, theta2, theta3, theta4];
-thick = [t1, t2, t3, t4];
+theta = deg2rad(45);
+thick = 0;
 
-a = zeros(3,3);         % Init A Matrix
-b = zeros(3,3);         % Init B Matrix
-d = zeros(3,3);         % Init D Matrix
+% Init A,B,D
+a = zeros(3,3);
+b = zeros(3,3);
+d = zeros(3,3);
 
 
+% Compliance Matrix [S]
 s11 = 1/E1; s22 = 1/E2; s66 = 1/G12;
 s12 = -nu_12/E1; 
 s21 = s12;
 
-q11 = inv(s11); q22 = inv(s22); q66 = inv(s66);
-q12 = inv(s12); q21 = q12;
+s_matrix = [s11 s12 0;
+            s21 s22 0;
+            0   0   s66];
 
 
+% Stiffness Matrix [Q]
+q_matrix = inv(s_matrix);
+
+q11 = q_matrix(1,1);
+q12 = q_matrix(1,2);
+q21 = q12;
+q22 = q_matrix(2,2);
+q66 = q_matrix(3,3);
+
+
+% Calculation of Stiffness Coefficients
 c = cos(theta);
 s = sin(theta);
 
 q11_bar = q11*c^4 + q22*s^4 + (2*q12 + 4*q66)*c^2*s^2;
-q12_bar = (q11 + q22 - 4*q66)*c^2*s^2 + q12*(c^4+s^4);
+q12_bar = (q11 + q22 - 4*q66)*(c^2)*(s^2) + q12*(c^4+s^4);
 q22_bar = q11*s^4 + q22*c^4 + (2*q12 + 4*q66)*c^2*s^2;
 q66_bar = (q11 + q22 - 2*q12 - 2*q66)*c^2*s^2 + q66*(c^4 + s^4);
 q16_bar = (q11 - 2*q66 - q12)*c^3*s - (q22 - q12 - 2*q66)*c*s^3;
 q26_bar = (q11 - 2*q66 - q12)*c*s^3 - (q22 - q12 - 2*q66)*c^3*s;
+
 
 % A Matrix Calculation
 % for i = 1:N
